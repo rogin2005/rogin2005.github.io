94099a949afb71458ee12861aced2eed36daba9d
Roger Ferreira Saraiva <rogerferreirasaraiva@gmail.com>
2026-06-30 14:20:46 -0300

    feat: add links para redes sociais, links services, correcoes layout

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 
+function abrirLink(service) {
+  const numero = "5585996139725";
+  const texto = encodeURIComponent(`Olá! Tenho interesse no seu serviço de ${service}. Poderia me dar mais informações?`);
+  window.open(`https://wa.me/${numero}?text=${texto}`, '_blank');
+}
+
+
 const btnMobile = document.querySelector('.btn-mobile');
 const navLinks = document.querySelector('.nav-links');
 
